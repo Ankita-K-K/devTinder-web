b9cb0c418841c649ec4f840bc89d8e2de8a17b25
Ankita <ankitakanakagiri@gmail.com>
2024-10-22 18:46:41 +0530

    daisyui-navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,9 @@ function App() {
         </div>
         <div className="flex-none gap-2">
           <div className="form-control">
-            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
           </div>
           <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar mx-6">
               <div className="w-10 rounded-full">
                 <img
                   alt="Tailwind CSS Navbar component"
